6cdb1b853c6e08634bebe1961631d1c0f6c80296
Geovane Borba <geovanne@live.com>
2022-04-30 19:27:43 -0300

    feat: add server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
 import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (request, response) => response.json({ message: 'Test'}))
+
+app.listen(3001, () => console.log('🚀 Back-end started on port 3000 🔥'))
